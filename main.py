9313d6e6a9033f2682eeb2afad6cf2996a745d37
João Pedro R. Diniz <jp_rd@hotmail.com>
2025-07-30 17:25:55 -0300

    main.py atualizado

--- main.py
+++ main.py
@@ -5,13 +5,28 @@ import nest_asyncio
 from dotenv import load_dotenv
 
 from modules.bot.indeed import IndeedBot, IndeedBotOptions
+from modules.bot.infojob import InfoJobBot, InfoJobBotOptions
 from modules.bot.linkedin import LinkedinBot, LinkedinBotOptions
 from modules.configs.indeed_config import IndeedConfig
+from modules.configs.infojob_config import InfoJobConfig
 from modules.configs.linkedin_config import LinkedinConfig
 
 nest_asyncio.apply()
 
 load_dotenv("../")
+
+
+async def infojob():
+    bot = "infojobbot"
+    configs = InfoJobConfig()
+    options = InfoJobBotOptions(
+        username=os.getenv("INFOJOB_USER"),
+        password=os.getenv("INFOJOB_PASS"),
+        searches=configs.get_searches(bot),
+        filters=configs.get_filters(),
+    )
+    bot = InfoJobBot(options)
+    await bot.start()
 
 
 async def indeed():
@@ -26,7 +41,6 @@ async def indeed():
     await bot.start()
 
 
-# TODO: Em homologação (requer testes)
 async def linkedin():
     bot = "linkedinbot"
     configs = LinkedinConfig()
@@ -41,4 +55,8 @@ async def linkedin():
 
 
 if __name__ == "__main__":
-    asyncio.run(indeed()) # FIXME: Implementar um listener para email para receber codigo de verificação
+    # Escolha um dos bots para executar
+    # asyncio.run(infojob())
+    # asyncio.run(indeed())
+    # asyncio.run(linkedin())
+    pass
